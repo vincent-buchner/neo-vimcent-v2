357fe8ed37b13081680d69ff76ff32d6dbadc89c
vincent-buchner <vincentbuchner01@gmail.com>
2025-08-10 17:25:19 -0500

    added floating terminal and better support for tsx files

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,8 +1,20 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- disable a keymap
-    keys[#keys + 1] = { "K", false }
-  end,
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- disable a keymap
+      keys[#keys + 1] = { "K", false }
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "tsx",
+        "typescript",
+      })
+    end,
+  },
 }
--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,6 +1,19 @@
 return {
-  { "akinsho/toggleterm.nvim", version = "*", opts = {
-    direction = "horizontal",
-    size = 15,
-  } },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      shade_terminals = true,
+      start_in_insert = true,
+      insert_mappings = true,
+      open_mapping = [[c-\>]],
+      direction = "float",
+      float_opts = {
+        border = "curved",
+        width = math.floor(vim.o.columns * 0.8),
+        height = math.floor(vim.o.lines * 0.8),
+        title_pos = "center",
+      },
+    },
+  },
 }
