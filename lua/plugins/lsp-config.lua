713f384ea12964bc94dbc1c8315217de332498bf
vincent-buchner <vincentbuchner01@gmail.com>
2026-02-04 18:58:36 -0600

    Updated config deps, using proper key disabling

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,11 +1,15 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- disable a keymap
-      keys[#keys + 1] = { "K", false }
-    end,
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            { "K", false }, -- disable
+          },
+        },
+      },
+    },
   },
   {
     "nvim-treesitter/nvim-treesitter",
